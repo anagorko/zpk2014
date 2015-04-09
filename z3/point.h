c9aacb2ce3a6b9467c463778d57f74523d33400e
Andrzej Nagórko <amn@mimuw.edu.pl>
2015-04-09 09:48:52 +0200

    przyklad dziedziczenia

--- z3/point.h
+++ z3/point.h
@@ -7,26 +7,22 @@ using namespace std;
 
 class Point
 {
-    double x, y, z;
+    double x, y;
 
 public:
     Point();
-    Point(double, double, double);
-
+    Point(double, double);
+    
     ~Point();
-
+    
     void setX(double);
     void setY(double);
-    void setZ(double);
-
+    
     double getX();
     double getY();
-    double getZ();
-
-    friend istream& operator>>(istream&, Point&);
+    
 };
 
 ostream& operator<<(ostream &, Point);
-istream& operator>>(istream &, Point&);
 
 #endif
