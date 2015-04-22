8ce2293593d389d60bcc279daf906653e35a8b3c
przemo1710wke@gmail.com <leggiero1710>
2015-04-22 23:58:47 +0200

    Praca domowa 6

---
+++ home/pwkaczmarek/z6/stos.cpp
@@ -0,0 +1,230 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+ * Obiekty klas implementuj¹cych interfejs ElementStosu mo¿na
+ * przechowywaæ w ni¿ej okreœlonej strukturze Stos.
+ */
+
+class ElementStosu
+{
+    virtual void output(ostream&) = 0;
+
+public:
+    virtual ~ElementStosu() = 0;
+
+    friend ostream& operator<<(ostream& os, ElementStosu& el);
+};
+
+ElementStosu::~ElementStosu() {}
+
+ostream& operator<<(ostream& os, ElementStosu& el)
+{
+    el.output(os);
+    return os;
+}
+
+/*
+ * Obiekty klasy Stos moga przechowywac dowolne obiekty implementujace interfejs
+ * ElementStosu.
+ */
+
+class Stos
+{
+    struct Link
+    {
+        ElementStosu    *dane;
+        Link            *poprzedni;
+    };
+
+    Link* wierzcholek;
+    int numer; // rozwiazanie problemu zliczania elementow
+
+public:
+    Stos() ;
+    ~Stos();
+    void push(ElementStosu* el);
+    ElementStosu* pop();
+    bool empty();
+    int size();
+
+    friend ostream& operator<<(ostream&, Stos &);
+};
+
+Stos::Stos()
+{
+    wierzcholek = NULL;
+    numer = 0;
+}
+
+Stos::~Stos()
+{
+    while(wierzcholek != NULL)
+    {
+        delete pop();
+    }
+    numer = 0;
+}
+
+void Stos::push(ElementStosu* el)
+{
+    Link* nowy = new Link;
+    nowy -> poprzedni = wierzcholek; // wskazniki
+    nowy -> dane = el;
+    wierzcholek = nowy;
+    numer++;
+}
+
+ElementStosu* Stos::pop()
+{
+    if(wierzcholek == NULL) //poczatek stosu
+        return NULL;
+
+    ElementStosu* wynik = wierzcholek -> dane;
+    Link* poprzedni = wierzcholek -> poprzedni;
+
+    delete wierzcholek;
+
+    wierzcholek = poprzedni;
+    numer--;
+
+    return wynik;
+}
+
+bool Stos:: empty()
+{
+    if(wierzcholek == NULL)
+        return true;
+    else
+        return false;
+}
+
+int Stos::size()
+{
+    return numer;
+}
+
+
+ostream& operator<<(ostream& os, Stos &s)
+{
+    os << "[";
+
+    Stos::Link *el = s.wierzcholek;
+    while(el != NULL)
+    {
+        os << *(el -> dane);
+        el = el -> poprzedni;
+        if(el != NULL)
+            cout << ", ";
+    }
+
+    os << "]";
+    return os;
+}
+
+/////////////////////////////////////////////////////////////////
+
+// Kalkulator
+
+
+class Liczba : public ElementStosu
+{
+    int l;
+    virtual void output(ostream& os)
+    {
+        os << l;
+    }
+
+public:
+    Liczba(int l)
+    {
+        set(l);
+    }
+
+    Liczba() : Liczba(0)
+    {
+    }
+
+    ~Liczba()
+    {
+    }
+
+    int get()
+    {
+        return l;
+    }
+
+    void set(int _l)
+    {
+        l = _l;
+    }
+};
+
+// program wyswietla stos, ale nie wykonuje obliczen
+int main()
+{
+    Stos s;
+
+    while (true) {
+        cout << s << endl;
+        cout << "> ";
+
+        string komenda;
+        cin >> komenda;
+
+        if (komenda == "q" || komenda == "quit") {
+            cout << "Bye" << endl;
+            return 0;
+        }
+
+        if (komenda == "+" || komenda == "*" || komenda == "-" || komenda == "/" || komenda == "%") {
+            Liczba *l2 = static_cast<Liczba*>(s.pop());
+            Liczba *l1 = static_cast<Liczba*>(s.pop());
+
+            if (l1 == NULL || l2 == NULL) {
+                cout << "Brak elementów na stosie." << endl; continue;
+            }
+
+            Liczba *wynik;
+
+            if (komenda == "+") {
+                wynik = new Liczba(l1 -> get() + l2 -> get());
+            } else if (komenda == "*") {
+                wynik = new Liczba(l1 -> get() * l2 -> get());
+            } else if (komenda == "-") {
+                wynik = new Liczba(l1 -> get() - l2 -> get());
+            } else if (komenda == "/") {
+                if (l2 -> get() == 0) {
+                    cout << "Nie umiem dzielic przez zero." << endl;
+
+                    wynik = new Liczba();
+                } else {
+                    wynik = new Liczba(l1 -> get() / l2 -> get());
+                }
+            } else if(komenda == "%") {
+                if(l2 -> get() == 0) {
+                    cout << "Nie umiem dzielic przez zero." << endl;
+                    wynik = new Liczba();
+                } else
+                    wynik = new Liczba(l1 -> get() % l2 -> get());
+            }
+
+
+            s.push(wynik);
+
+            delete l1;
+            delete l2;
+
+            continue;
+        }
+
+        try {
+            int l = stoi(komenda);
+
+            s.push(new Liczba(l));
+        } catch (string ex) {
+            cout << "Nieznana komenda: " << komenda << endl;
+        }
+    }
+}
