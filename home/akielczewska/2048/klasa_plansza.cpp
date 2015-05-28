b9863818fd53689e196152e8eca6aedbe5da5921
akielczewska <aneta.kielczewska@student.uw.edu.pl>
2015-05-28 12:22:50 +0200

    projekt - update

---
+++ home/akielczewska/2048/klasa_plansza.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Plansza {
+
+    int t[4][4];
+
+public:
+
+
+};
+
+int main() {
+
+
+}
--- home/akielczewska/2048/ruch_w_gore.cpp
+++ home/akielczewska/2048/ruch_w_gore.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main() {
     // tablica 4x4
     int t[4][4];
 
-    // wypelnienie tablicy przykladowymi liczbami - same zera
-    for (int i=0; i<4; i++) {
-        for (int j=0; j<4; j++) {
-            t[i][j] = 0;
-        }
-    }
-
-
-    // wypelnienie tablicy przyk³adowymi liczbami - inny sposób
+    // wypelnienie tablicy przykladowymi liczbami
     // tutaj podstawialam sobie ró¿ne opcje, by sprawdzac poprawnosc funkcji
-t[0][0] = 2; t[0][1] = 0; t[0][2] = 4; t[0][3] = 8;
-t[1][0] = 2; t[1][1] = 2; t[1][2] = 4; t[1][3] = 2;
-t[2][0] = 4; t[2][1] = 2; t[2][2] = 2; t[2][3] = 4;
-t[3][0] = 4; t[3][1] = 2; t[3][2] = 16; t[3][3] = 4;
+    t[0][0] = 2; t[0][1] = 0; t[0][2] = 4; t[0][3] = 8;
+    t[1][0] = 2; t[1][1] = 2; t[1][2] = 4; t[1][3] = 2;
+    t[2][0] = 4; t[2][1] = 2; t[2][2] = 2; t[2][3] = 4;
+    t[3][0] = 4; t[3][1] = 2; t[3][2] = 16; t[3][3] = 4;
 
-
-    // wyswietlenie tablicy w konsoli
+// wyswietlenie tablicy w konsoli
     cout << "Poczatkowa plansza:" << endl;
     for (int i=0; i<4; i++) {
         for (int j=0; j<4; j++) {
@@ -31,7 +24,8 @@ t[3][0] = 4; t[3][1] = 2; t[3][2] = 16; t[3][3] = 4;
     }
     cout << endl;
 
-    // przesuniêcie w góre (bez sklejania)
+
+    // przesuniecie w góre (bez sklejania)
     // kolejno dla ka¿dej kolumny j
     for (int j=0; j<4; j++) {
 /* nie robie petli dla kazdego wiersza - bo i tak kazdy mialby inne warunki -
@@ -118,4 +112,5 @@ t[3][0] = 4; t[3][1] = 2; t[3][2] = 16; t[3][3] = 4;
         cout << endl;
         }
         cout << endl;
+
 }
