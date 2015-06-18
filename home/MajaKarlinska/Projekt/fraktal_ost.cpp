71dc014de1e69ed407abfa2bddea7e6c91c7be64
Andrzej Nagórko <amn@mimuw.edu.pl>
2015-06-18 18:13:57 +0200

    obsluga myszki

--- home/MajaKarlinska/Projekt/fraktal_ost.cpp
+++ home/MajaKarlinska/Projekt/fraktal_ost.cpp
@@ -1,7 +1,9 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
 #include <iostream>
 #include <math.h>
 using namespace std;
-#include "display.h"
 #include "stdio.h"
 
 double cXmin = -2, cYmin = -1.5;
@@ -20,11 +22,13 @@ double translateY(double _y)
 	return ((_y+300)/600*(cYmax-cYmin))+cYmin;
 	}
 
+const int max_iters = 255;
+
 int calculate (double _x, double _y)
 	{
 	int n=0;
-	int imax=30;
-	double rmax = 0.3;
+	int imax=max_iters;
+	double rmax = 3*sqrt(_x*_x + _y*_y);
 	double x=0;
 	double y=0;
 	double xx=0;
@@ -35,11 +39,10 @@ int calculate (double _x, double _y)
 		y=2*x*y+_y;
 		x=xx;
 		r=sqrt(x*x+y*y);
-		if (r < rmax)
+		if (r > rmax)
 			break;
 		}
-	if (n==imax) return 0;
-	return 1;
+        return imax - n;
 	}
 
 double color (double _x, double _y)
@@ -47,21 +50,79 @@ double color (double _x, double _y)
 	return calculate(translateX(_x),translateY(_y));
 	}
 
-int main ()
-	{
-	Display *d = new Display();
-	int c = 0;
-	d->init();
-	d->clear();
-	for (_y=-300; _y<300 ; _y++)
-		{
-		for (_x=-400; _x<400; _x++)
-			{
-			c = color(_x, _y);
-			if (c>0) d->drawPoint ( _x, _y);
-			}
+int main (int, char**)
+{
+	ALLEGRO_DISPLAY *display = NULL;
+
+	if(!al_init()) {
+        	cerr << "Błąd podczas inicjalizacji allegro." << endl;
+        	assert(false);
+    	}
+
+    	if (!al_init_primitives_addon()) {
+        	cerr << "Błąd podczas inicjalizacji dodatku 'primitives'." << endl;
+        	assert(false);
+    	}
+
+    	display = al_create_display(800, 600);
+
+    	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+        if(!al_install_mouse()) {
+                cout <<  "failed to initialize the mouse!" << endl;
+                return -1;
+        }
+        event_queue = al_create_event_queue();
+        if (!event_queue) {
+                cout << "fail" << endl;
+                return -1;
+        }
+        al_register_event_source(event_queue, al_get_mouse_event_source());
+        
+    	while (true)
+    	{
+                ALLEGRO_EVENT ev;
+                al_wait_for_event(event_queue, &ev);
+
+                if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+                        cout << "wcisniety klawisz " << ev.mouse.x << " " << ev.mouse.y << endl;
+
+                        double x = ev.mouse.x;
+                        double y = ev.mouse.y;
+                        
+                        double pixelWidth = (cXmax - cXmin) / 800.0;
+                        double pixelHeight = (cYmax - cYmin) / 600.0;
+                        
+                        double newWidth = (cXmax - cXmin) / 4.0;
+                        double newHeight = (cYmax - cYmin) / 4.0;
+                        
+                        
+                        cXmin = (cXmin + x * pixelWidth) - newWidth / 2;
+                        cYmin = (cYmin + y * pixelHeight) - newHeight / 2;
+                        
+                        cXmax = cXmin + newWidth;
+                        cYmax = cYmin + newHeight;
+                }
+                
+                if (al_is_event_queue_empty(event_queue)) {
+                    	// rysowanie fraktala
+                    	int c = 0;
+                    	al_clear_to_color(al_map_rgb(0,0,0));
+                    	for (_y=-300; _y<300 ; _y++)
+		        {
+		                for (_x=-400; _x<400; _x++)
+			        {
+				        c = color(_x, _y);
+				        if (c>0) {
+				                al_draw_pixel(_x+800/2,_y+600/2, al_map_rgb(c * 255 / max_iters,c * 255 / max_iters, c * 255 / max_iters));
+				                }
+                                }
+                        }
+        		al_flip_display();
+
+        		cout << "Rozmiary: " << cXmin << " " << cYmin << " " << cXmax << " " << cYmax << endl;
 		}
-	d->show();
-	system("pause");
+		
+        }
+        
 	return 0;
-	}
+}
