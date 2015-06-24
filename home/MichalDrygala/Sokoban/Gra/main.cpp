ba8d146759349c46b0a938d5e274eb1366b13a25
plemberski <piotr.lemberski@gmail.com>
2015-06-24 04:28:42 +0200

    muzyka

--- home/MichalDrygala/Sokoban/Gra/main.cpp
+++ home/MichalDrygala/Sokoban/Gra/main.cpp
@@ -2,6 +2,9 @@
 #include <allegro5/allegro_font.h>
 #include<allegro5/allegro_image.h>
 #include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
 
 #include <iostream>
 using namespace std;
@@ -22,6 +25,9 @@ int czas = 0;
 
 void rysuj_ruchome(clsLudzik, clsSkrzynka, int);
 void ruchy(clsPlansza& plansza1, clsLudzik& on, clsSkrzynka& s);
+int menu_przyciski();
+
+int WybierzLevel();
 
 int main(){
 
@@ -31,6 +37,8 @@ int main(){
 
 //inicjalizacja czcionek
     al_init_font_addon();
+//al_install_audio();//bład biblioteki
+//al_init_acodec_addon();
 
 //inicjalizacja allegro + klawiatury + obrazow + czcionek ttf
     if (!al_init() || !al_install_keyboard()  || !al_init_image_addon() || !al_init_ttf_addon())
@@ -46,6 +54,14 @@ int main(){
     ALLEGRO_FONT *font = al_load_ttf_font("arial.ttf", 12, 0 );
 
  al_set_window_title( display,"SOKOBAN VERSION 3.0 Drygala & Lemberski");//nazwa okna
+
+
+//ALLEGRO_SAMPLE *songE = al_load_sample("songE.ogg");
+//al_reserve_samples(1);
+//ALLEGRO_SAMPLE_INSTANCE *songInstance = al_create_sample_instance(song);
+//al_set_sample_instance_playmode(songInstance, ALLEGRO_PLAYMODE_LOOP);
+//al_attach_sample_instance_to_mixer(songInstance, al_get_default_mixer());
+//al_play_sample_instance(songInstance);
 
 //sprawdzenie poprawnosci wskaznikow
     if (display == NULL || timer == NULL || event_queue == NULL || font == NULL)
@@ -68,11 +84,11 @@ int main(){
 /* ******************************************************************************************************************* */
 
     clsMenu objMenu;
-    //objMenu.WyswietlKomunikat(font);
     objMenu.WyswietlMenu();
-
-    int a;
-    cin >> a;
+int a;
+cin >> a;
+//al_play_sample(songE,1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE,0);
+    //cout << WybierzLevel();
 
 
 //<> wybiera odpowiedni¹ planszê (decyduje u¿ytkownik)
@@ -132,12 +148,31 @@ int main(){
         }
     }
 
+
+
+
     return 0;
 }
 
 /* ******************************************************************************************************************************************* */
 /* FUNKCJE                                                                                                                                     */
 /* ******************************************************************************************************************************************* */
+/*
+int menu_przyciski()
+{
+    if (key[ALLEGRO_KEY_1] || key[ALLEGRO_KEY_PAD_1])
+    {
+        return 1;
+    cout << "1111111111" << endl;
+    }
+
+    if (key[ALLEGRO_KEY_2] || key[ALLEGRO_KEY_PAD_2])
+    {
+        return 2;
+    cout << "222" << endl;
+    }
+
+} */
 
 void ruchy(clsPlansza& plansza1, clsLudzik& on, clsSkrzynka& s)
 {
@@ -150,6 +185,7 @@ void ruchy(clsPlansza& plansza1, clsLudzik& on, clsSkrzynka& s)
         if ((plansza1.get_tblPodloga(x, y - 1)== 0 || plansza1.get_tblPodloga(x, y - 1)== 6) && (s.get_tblSkrzynkiS(x, y - 1) == 0))
         {
             energia = 0; y--;
+
         }
         else if ((s.get_tblSkrzynkiS(x, y - 1) == 1) && (s.get_tblSkrzynkiS(x, y - 2) == 0) && (plansza1.get_tblPodloga(x, y - 2) == 0 || plansza1.get_tblPodloga(x, y - 2) == 6))
         {
@@ -209,3 +245,48 @@ void ruchy(clsPlansza& plansza1, clsLudzik& on, clsSkrzynka& s)
     on.set_Y(y);
     on.set_energia(energia);
 }
+
+/*
+int WybierzLevel()
+{
+
+    bool wyjdz = false;
+
+    while(!wyjdz)
+    {
+
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+
+        if(ev.type == ALLEGRO_EVENT_TIMER)  // minęła 1/60 (1/FPS) część sekundy
+        {
+          if (key[ALLEGRO_KEY_1] || key[ALLEGRO_KEY_PAD_1])
+            {
+                return 1;
+                //cout << "1111111111" << endl;
+            }
+
+            if (key[ALLEGRO_KEY_2] || key[ALLEGRO_KEY_PAD_2])
+            {
+                    return 2;
+                //cout << "222" << endl;
+            }
+
+        }
+        else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+        {
+            key[ev.keyboard.keycode] = true;
+        }
+        else if (ev.type == ALLEGRO_EVENT_KEY_UP)
+        {
+            key[ev.keyboard.keycode] = false;
+
+            if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+            {
+                //wyjdz = true;
+                return 0;
+            }
+        }
+    }
+}
+*/
